d430705ff5a28f220459ddbb9940d95772ece6f2
Daniel Meza <daniel.meza29@yahoo.com>
2020-04-20 11:14:11 -0500

    Improved naming properties

--- scripts/journal.js
+++ scripts/journal.js
@@ -3,22 +3,22 @@
     represents a journal entry about what you learned today
 */
 const journalEntry1 = {
-  "Date of Entry": "04/17/2020",
-  "Concepts covered": "Manipulating the DOM",
-  "Journal Entry": "It was great learning how to make websites a little more dynamic, goodbye static websites!",
-  "Mood of the day": "Excited"
+  date: "04/17/2020",
+  concepts: "Manipulating the DOM",
+  entry: "It was great learning how to make websites a little more dynamic, goodbye static websites!",
+  mood: "Excited"
 }
 const journalEntry2 = {
-  "Date of Entry": "04/17/2020",
-  "Concepts covered": "JavaScript Objects",
-  "Journal Entry": "Good review for objects, it's slowly coming back",
-  "Mood of the day": "Satisfied"
+  date: "04/17/2020",
+  concepts: "JavaScript Objects",
+  entry: "Good review for objects, it's slowly coming back",
+  mood: "Satisfied"
 }
 const journalEntry3 = {
-  "Date of Entry": "04/16/2020",
-  "Concepts covered": "Intro to Javascript",
-  "Journal Entry": "Today we covered so much. I feel like I learned so much and it's excited getting into Javascript!",
-  "Mood of the day": "Excited"
+  date: "04/16/2020",
+  concepts: "Intro to Javascript",
+  entry: "Today we covered so much. I feel like I learned so much and it's excited getting into Javascript!",
+  mood: "Excited"
 }
 
 const journalCollection = [];
