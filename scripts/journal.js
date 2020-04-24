6e3e01392a06e8683147dc909efca7f0aa3ba244
Daniel Meza <daniel.meza29@yahoo.com>
2020-04-23 22:20:07 -0500

    Fetching data from my json and displaying in the DOM

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,52 +1,20 @@
-/*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
-const journalEntry1 = {
-  date: "04/17/2020",
-  concepts: "Manipulating the DOM",
-  entry: "It was great learning how to make websites a little more dynamic, goodbye static websites!",
-  mood: "excited"
-}
-const journalEntry2 = {
-  date: "04/17/2020",
-  concepts: "JavaScript Objects",
-  entry: "Good review for objects, it's slowly coming back.",
-  mood: "satisfied"
-}
-const journalEntry3 = {
-  date: "04/16/2020",
-  concepts: "Intro to Javascript",
-  entry: "Today we covered so much. I feel like I learned so much and it's excited getting into Javascript!",
-  mood: "excited"
-}
-
-//Adds all my journal entries and adds them to journalCollection
-const journalCollection = [];
-journalCollection.push(journalEntry1);
-journalCollection.push(journalEntry2);
-journalCollection.push(journalEntry3);
-
 //In html my first entryLog(and only) gets assigned journalContainer
 const journalContainer = document.querySelector(".entryLog");
 
+// HTML structure for a journal entry
 const makeJournalEntryComponent = (journalEntry) => {
-  // HTML structure for a journal entry
   return `
     <div>On ${journalEntry.date}, I learned about ${journalEntry.concepts}. 
     ${journalEntry.entry} I am ${journalEntry.mood}.</div>
   `
 }
 
-/*
-    Purpose: To render all journal entries to the DOM
-    Arguments: entries (array of objects)
-*/
-const renderJournalEntries = (entries) => {
-  for (let entry of journalCollection) {
-    journalContainer.innerHTML += makeJournalEntryComponent(entry);
-  }
-}
-
-// Invoke the render function
-renderJournalEntries(journalCollection);
+fetch("http://localhost:3000/journalEntries")
+  .then(journalEntries => journalEntries.json())  // Parse as JSON
+  .then(entries => {
+    //Iterating through my entries from json that now is in JS and adding
+    //every entry by calling makeJournalEntryComponent
+    for (let entry of entries) {
+      journalContainer.innerHTML += makeJournalEntryComponent(entry);
+    }
+  })
