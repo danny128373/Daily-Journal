90230bc13080af7a778511d46c1ca1371d826ac6
Daniel Meza <daniel.meza29@yahoo.com>
2020-04-26 19:34:45 -0500

    Included import and exports

---
+++ scripts/data.js
@@ -0,0 +1,15 @@
+import { journalContainer, makeJournalEntryComponent } from "./entryComponent.js";
+
+const fetchJournalEntries = () => {
+  fetch("http://localhost:8088/journalEntries")
+    .then(journalEntries => journalEntries.json())  // Parse as JSON
+    .then(entries => {
+      //Iterating through my entries from json that now is in JS and adding
+      //every entry by calling makeJournalEntryComponent
+      for (let entry of entries) {
+        journalContainer.innerHTML += makeJournalEntryComponent(entry);
+      }
+    })
+}
+
+export { fetchJournalEntries };
--- scripts/journal.js
+++ scripts/journal.js
@@ -1,20 +1,3 @@
-//In html my first entryLog(and only) gets assigned journalContainer
-const journalContainer = document.querySelector(".entryLog");
+import { fetchJournalEntries } from "./data.js";
 
-// HTML structure for a journal entry
-const makeJournalEntryComponent = (journalEntry) => {
-  return `
-    <div>On ${journalEntry.date}, I learned about ${journalEntry.concepts}. 
-    ${journalEntry.entry} I am ${journalEntry.mood}.</div>
-  `
-}
-
-fetch("http://localhost:3000/journalEntries")
-  .then(journalEntries => journalEntries.json())  // Parse as JSON
-  .then(entries => {
-    //Iterating through my entries from json that now is in JS and adding
-    //every entry by calling makeJournalEntryComponent
-    for (let entry of entries) {
-      journalContainer.innerHTML += makeJournalEntryComponent(entry);
-    }
-  })
+fetchJournalEntries();
