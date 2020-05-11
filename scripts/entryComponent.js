49cdff09c3feeea44f0170bee5286e8089714a12
Daniel Meza <daniel.meza29@yahoo.com>
2020-05-11 11:34:08 -0500

    Added edit button to the DOM, no functionality yet

--- scripts/entryComponent.js
+++ scripts/entryComponent.js
@@ -8,6 +8,9 @@ const makeJournalEntryComponent = (journalEntry) => {
   return `
     <div>On ${journalEntry.date}, I learned about ${journalEntry.concepts}. 
     ${journalEntry.entry} I am ${journalEntry.mood}.</div>
+    <button id="editEntry--${journalEntry.id}">
+      Edit Entry
+    </button>
     <button id="deleteEntry--${journalEntry.id}">
             Delete Entry
     </button>
